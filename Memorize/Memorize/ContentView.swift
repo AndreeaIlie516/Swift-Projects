b19b32cc9bfb4f615c37feb8c785ba409ed63be5
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2024-02-06 00:29:09 +0200

    Assignment 1 done

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,25 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["⛰️", "🍃", "🦋", "🌊", "🌸", "🦔","🏕️", "🐌", "🏝️", "🍄", "🐚", "🪺", "🪷", "🐝", "🌳", "🐬", "🪸", "🦚"]
     
-    @State var cardCount: Int = 4
+    enum Theme: Int, CaseIterable {
+            case nature, food, sport
+            
+            var name: String {
+                switch self {
+                case .nature: return "Nature"
+                case .food: return "Food"
+                case .sport: return "Sport"
+                }
+            }
+            
+            var symbol: String {
+                switch self {
+                case .nature: return "mountain.2.fill"
+                case .food: return "carrot.fill"
+                case .sport: return "basketball.fill"
+                }
+            }
+            
+            var emojis: [String] {
+                switch self {
+                case .nature: return ["⛰️", "🍃", "🦋", "🌊", "🌸", "🦔","🏕️", "🐌", "🏝️", "🍄", "🐚", "🪺", "🪷", "🐝", "🌳", "🐬", "🪸", "🦚"]
+                case .food: return ["🍉", "🍕", "🌮", "🥐", "🍓", "🍔", "🥟", "🍰", "🍤", "🥗", "🥨", "🥞", "🫐"]
+                case .sport: return ["⚽️", "🏓", "⛸️", "🏈", "🎾", "🛹", "🏸", "🥍", "🏐", "🏊‍♂️", "🏹", "🧗‍♀️"]
+                }
+            }
+        }
+    
+    
+    @State var cardCount: Int = 8
+    @State var theme: Theme = .nature
+    @State var emojis: [String] = Theme.nature.emojis
     
     var body: some View {
         VStack {
+            Text("Memorize!").font(.largeTitle)
             ScrollView {
                 cards
             }
             cardCountAdjusters
+            themeAdjusters
         }
         .padding()
+        .onAppear {
+            switchTheme(to: theme)
+        }
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(),GridItem()]) {
+        LazyVGrid(columns: [GridItem(),GridItem(), GridItem(), GridItem()]) {
             ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
+                
             }
         }
         .foregroundColor(.mint)
@@ -45,18 +81,57 @@ struct ContentView: View {
     func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
         Button(action: {
             cardCount += offset
+            shuffleEmojis(to: theme)
         }, label: {
             Image(systemName: symbol)
         })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+        .disabled(cardCount + offset < 8 || cardCount + offset > theme.emojis.count * 2)
     }
     
     var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+        cardCountAdjuster(by: -2, symbol: "minus.circle")
     }
     
     var cardAdder: some View {
-        cardCountAdjuster(by: +1, symbol:  "rectangle.stack.badge.plus.fill")
+        cardCountAdjuster(by: +2, symbol:  "plus.circle")
+    }
+    
+    var themeAdjusters: some View {
+        HStack {
+            Spacer()
+            ForEach(Theme.allCases, id: \.self) {
+                themeOption in
+                themeAdjuster(for: themeOption)
+                Spacer()
+            }
+
+        }
+        .imageScale(.large)
+        .padding(.horizontal, 70)
+    }
+    
+    func themeAdjuster(for theme: Theme) -> some View {
+        VStack {
+            Button(action: {
+                switchTheme(to: theme)
+            }, label: {
+                Image(systemName: theme.symbol)
+            })
+            Text(theme.name).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        }
+    }
+    
+    func switchTheme(to newTheme: Theme) {
+        theme = newTheme
+        shuffleEmojis(to: newTheme)
+        cardCount = min(cardCount, emojis.count)
+    }
+    
+    func shuffleEmojis(to theme: Theme) {
+        let shuffledEmojis = theme.emojis.shuffled()
+        let numberOfUniqueEmojisNeeded = cardCount / 2
+        let emojiSubset = Array(shuffledEmojis.prefix(numberOfUniqueEmojisNeeded))
+        emojis = (emojiSubset + emojiSubset).shuffled()
     }
 }
 
